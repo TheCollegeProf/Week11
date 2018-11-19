1c8119bcb35596eb09cb4a11b9d77ce3b7edefcd
TheCollegeProf <keith.connell@georgiancollege.ca>
2018-11-19 11:52:26 -0500

    first commit

---
+++ Week11 InClass/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Week11_InClass
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.cmbRooms = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(133, 333);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(157, 42);
+            this.btnExecute.TabIndex = 0;
+            this.btnExecute.Text = "Execute";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(133, 113);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 26);
+            this.txtName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Please enter your name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(256, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "How many bedrooms do you have?";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(133, 399);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 20);
+            this.lblOutput.TabIndex = 5;
+            // 
+            // cmbRooms
+            // 
+            this.cmbRooms.FormattingEnabled = true;
+            this.cmbRooms.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8"});
+            this.cmbRooms.Location = new System.Drawing.Point(133, 191);
+            this.cmbRooms.Name = "cmbRooms";
+            this.cmbRooms.Size = new System.Drawing.Size(121, 28);
+            this.cmbRooms.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbRooms);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnExecute);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.ComboBox cmbRooms;
+    }
+}
+
---
+++ Week11 InClass/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Week11_InClass
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            String name = txtName.Text;
+            String rooms = cmbRooms.Text;
+
+            //int roomCount;
+            //bool isNum = int.TryParse(rooms, out roomCount);
+
+            //if(isNum && roomCount > 0)
+            //{
+            // Output the variables per instruction
+            lblOutput.Text = "Hello " + name + " your home has " + rooms + " bedrooms.";
+            //}
+
+            //else
+            //{
+            //    MessageBox.Show("Invalid entry, please try again");
+            //    txtRooms.Clear();
+            //}
+
+
+        }
+    }
+}
